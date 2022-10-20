8442a06642f1a106680b735ce74bea10da90694a
Fernando Dingler <ferdingler@users.noreply.github.com>
2022-10-20 13:35:59 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Today is Oct 14, 2022. This is a web preview
+          Today is Oct 20, 2022. This is a web preview
         </p>
         <a
           className="App-link"
