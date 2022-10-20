bf07c35eb1692740a8008e003f532175d2a88af4
Fernando Dingler <ferdingler@users.noreply.github.com>
2022-10-20 15:21:38 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Today is Oct 20, 2022 in Seattle, WA
+          Today is Oct 20, 2022 in San Diego, CA
         </p>
         <a
           className="App-link"
